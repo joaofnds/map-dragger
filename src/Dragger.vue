b97ea8865626bd3ce9e23c9a20144067bcd228e8
João Fernandes <joaovitorfernandes2@gmail.com>
2017-01-25 12:32:29 -0200

    Intial commit. Draggable map only

---
+++ src/Dragger.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<img id='map' draggable='false' src="./assets/map.jpg" alt="Vuejs Logo">
+	</div>
+</template>
+
+<script>
+
+function boundaryCalculator(map) {
+	let top = innerHeight - map.height()
+	let left = innerWidth - map.width()
+	return [top, 0, 0, left]
+
+}
+
+jQuery(function($) {
+	const $map = $('#map')
+
+	const constrainTo = boundaryCalculator($map)
+	console.log(constrainTo)
+
+	$map.pep({
+		constrainTo
+	}); 
+});
+
+export default {
+	name: 'dragger',
+}
+</script>
+
+<style>
+	html, body {
+		overflow: hidden;
+	}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Dragger from './Dragger.vue'
+
+new Vue({
+  el: '#dragger',
+  render: h => h(Dragger)
+})
